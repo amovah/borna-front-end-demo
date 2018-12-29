a617e7c7e3d1dd3226b9e001e88188c562801090
Ali Movahedi <ali_movahedi@aol.com>
2018-12-29 09:56:01 +0330

    fix: fix all mask

--- src/shared/components/mine/DateMask.jsx
+++ src/shared/components/mine/DateMask.jsx
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react';
 import { enToFa, faToEn } from 'Root/mapper';
 import MaskedInput from 'react-text-mask';
 
+const regex = [/[۰-۹]|[0-9]/, /[۰-۹]|[0-9]/, /[۰-۹]|[0-9]/, /[۰-۹]|[0-9]/, '/',  /[۰-۱]|[0-1]/, /[۰-۹]|[0-9]/, '/', /[۰-۳]|[0-3]/, /[۰-۹]|[0-9]/] // eslint-disable-line
+
 export default class extends PureComponent {
   handleChange = (event) => {
     this.props.input.onChange(enToFa(event.target.value));
@@ -19,7 +21,7 @@ export default class extends PureComponent {
         value={input.value}
         placeholder={placeholder || '__/__/____'}
         type={type}
-        mask={mask}
+        mask={regex}
         onChange={this.handleChange}
         disabled={this.props.disabled}
       />
