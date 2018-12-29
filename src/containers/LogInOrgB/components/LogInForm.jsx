a6c60cc879882f5f1ed756f54f12cbe7c401a3b8
Ali Movahedi <ali_movahedi@aol.com>
2018-12-29 10:08:30 +0330

    fix: fix login page add sample image

--- src/containers/LogInOrgB/components/LogInForm.jsx
+++ src/containers/LogInOrgB/components/LogInForm.jsx
@@ -6,6 +6,7 @@ import AccountOutlineIcon from 'mdi-react/AccountOutlineIcon';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 // import titleLogo from 'shared/img/logo.png';
+import titleLogo from 'Root/shared/img/logo/logo.png';
 
 class LogInForm extends PureComponent {
   static propTypes = {
@@ -32,10 +33,10 @@ class LogInForm extends PureComponent {
     return (
       <form className="form" onSubmit={handleSubmit}>
         <div className="login-page-title">
-          {/* <img
+          <img
             src={titleLogo}
             alt="khafes"
-          /> */}
+          />
           <h1>
             دومیش
           </h1>
