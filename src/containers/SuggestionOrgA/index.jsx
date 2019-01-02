13ed4d2f77281646a4b2caf993626c729291ba95
Ali Movahedi <ali_movahedi@aol.com>
2019-01-02 20:30:53 +0330

    feat: add filter to user Control

--- src/containers/SuggestionOrgA/index.jsx
+++ src/containers/SuggestionOrgA/index.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import { Container, Row } from 'reactstrap';
 import MatTable from './components/MatTable';
 import HeadFilter from './components/HeadFilter';
-
-const showres = (values) => {
-  console.log(values);
-};
+import filterSug from 'Root/redux/actions/userOrgC/filterSug';
 
 const MaterialTable = () => (
   <Container>
     <div>
-      <HeadFilter onSubmit={showres} />
+      <HeadFilter onSubmit={filterSug} />
     </div>
     <Row>
       <MatTable />
