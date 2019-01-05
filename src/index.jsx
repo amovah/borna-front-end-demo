86c66646a31c82d241b3759f67dfb5e34df31795
Ali Movahedi <ali_movahedi@aol.com>
2019-01-05 16:29:54 +0330

    feat: load data from server when you are logged already

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,21 @@ import { render } from 'react-dom';
 import App from './containers/App/App';
 import store from 'Root/store';
 import types from 'Root/redux/actions';
+import loadClient from 'Root/redux/actions/userOrgB/loadClient';
+import loadDeposits from 'Root/redux/actions/userOrgB/loadDeposits';
+import loadTokens from 'Root/redux/actions/userOrgC/loadTokens';
+import loadTrans from 'Root/redux/actions/userOrgC/loadTrans';
+import loadClientA from 'Root/redux/actions/userOrgC/loadClient';
+import loadSuggestion from 'Root/redux/actions/userOrgC/loadSuggestion';
 
 if (localStorage.orgB) {
   store.dispatch({
     type: types.userOrgB.LOGIN,
     ...JSON.parse(localStorage.orgB),
   });
+
+  loadDeposits();
+  loadClient();
 }
 
 if (localStorage.orgC) {
@@ -16,6 +25,11 @@ if (localStorage.orgC) {
     type: types.userOrgC.LOGIN,
     ...JSON.parse(localStorage.orgC),
   });
+
+  loadClientA();
+  loadTokens();
+  loadTrans();
+  loadSuggestion();
 }
 
 render(
