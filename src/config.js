1f2872fc4edc0160dae72d8186ff0a04e396a268
MrWooJ <hurricanc@gmail.com>
2019-01-20 14:16:14 +0000

    Change IP Address of Server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import { env } from 'process';
 
 export default {
-  server: 'http://66.70.130.225:3010/api/',
+  server: 'http://66.70.130.224:3010/api/',
   // server:'http://localhost:3010/api/',
 };
